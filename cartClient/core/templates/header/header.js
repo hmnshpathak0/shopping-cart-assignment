761962cd1bcb88bd7508609a80411d649bb9bfd6
Himanshu Pathak <himpatha2@publicisgroupe.net>
2020-01-08 16:56:20 +0530

    changes done for desktop version in header

--- cartClient/core/templates/header/header.js
+++ cartClient/core/templates/header/header.js
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
 import './header.scss';
+import  {NavLink} from 'react-router-dom'
 import CartButton from '../../theme/buttons/cartButton/cartButton'
 import DropDown from '../../theme/dropDown/dropdown';
+import links from './links.json'
 
 class Header extends React.Component{
     constructor(){
         super();
         this.state={
-            toggle:false
+            toggle:false,
+            height:0
         }
+        this.headerElement = React.createRef();
+        links.slice(0,2).map(url => console.log(url.url))
     }
+  
     //this method is called to toggle the menu Bar
     toggleMenu=() => {
        
         this.setState({toggle : !this.state.toggle})
     }
+    updateDimensions(){
+        console.log("height",this.headerElement)
+        this.setState({height:this.ref.current.clientHeight})
+    }
+    //Calling the componentDidMount life cycle
+    componentDidMount(){
+        
+        window.addEventListener('resize', this.updateDimensions);
+    }
     render(){
         
         return(
-        <div className='header'>
-        <div  className='header_leftpan'>
-        <img className='header_logo' alt='sabka Bazaar'/>
-        <i  onClick={this.toggleMenu} aria-hidden="true" className='fa fa-bars fa-2x'></i>
+        <div ref={this.headerElement} className='header'>
+        <nav role="navigation" className='header_leftpan'>
+        <img className='header_logo' title='sabka Bazaar' alt='sabka Bazaar'/>
+        <i title='Menu bar' tabIndex='0'  role="navigation" onClick={this.toggleMenu} aria-hidden="true" className='fa fa-bars fa-2x'></i>
         <div className='header_menu'>
-        <li><a>Home</a></li>
-        <li><a>Products</a></li>
+            {links.slice(0,2).map((nav,index) =>{
+               return  <NavLink aria-label={nav.name} key={index}  className='header_menu_item' to={'/'+nav.url}>{nav.name}</NavLink>
+            })
+        }
         </div>
-        <DropDown toggle={this.state.toggle}/>
-        </div>
+        <DropDown height={this.state.height} toggle={this.state.toggle}/>
+        </nav>
         <div className='header_rightpan'>
             <CartButton/>
         </div>
--- cartClient/core/theme/dropDown/dropdown.js
+++ cartClient/core/theme/dropDown/dropdown.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './DropDown.scss';
+import {NavLink} from 'react-router-dom';
+import links from  '../../templates/header/links.json'
 
 function DropDown(props) {
   return (
       
-    <div className={props.toggle?"header_dropdown show":"header_dropdown"}>
-    <a href="home">Home</a>
-    <a href="home">Products</a>
-    </div>
+    <div style={{transform:"3D(0,"+props.height+",0)"}} className={props.toggle?"header_dropdown header_dropdown-show":"header_dropdown"}>
+   
+   {links.map((nav,index) =>{
+               return  <NavLink aria-label={nav.name} key={index}  to={'/'+nav.url}>{nav.name}</NavLink>
+            })
+        }
+        </div>
   );
 }
 
